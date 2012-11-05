ec2314b4eaf1cd1bf0af3bc86841470f72ea1c37
HaifaCarina <haifa@baluyos.net>
2012-11-06 00:48:29 +0800

    Removed all PDF files

---
+++ wokaholic/TestViewController.m
@@ -0,0 +1,43 @@
+//
+//  TestViewController.m
+//  wokaholic
+//
+//  Created by Haifa Carina Baluyos on 11/5/12.
+//  Copyright (c) 2012 NMG Resources, Inc. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
